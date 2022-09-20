45020e86298804d2cd7e70f7aa01aa27a60876a6
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-20 13:12:43 -0500

    Header and Navigation Bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,15 @@ import LogoBrand from '../img/new-logo.png';
 const Header = () => {
     return (
         <div className='header'>
-            <Header>
-                <img 
-                    src={LogoBrand} 
-                    alt="podcast logo brand" 
-                    width='300px'
-                    height='300px'
-                    className='mt-5 mb-4 logo-img'
-                />
+            <img 
+                src={LogoBrand} 
+                alt="podcast logo brand" 
+                width='300px'
+                height='300px'
+                className='mt-5 mb-4 logo-img'
+            />
 
-                <h1 className='main-header text-center pb-4'>Talk Your FEMSH!T</h1>
-            </Header>
+            <h1 className='main-header text-center pb-4'>Talk Your FEMSH!T</h1>
         </div>
     )
 }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,39 +1,31 @@
-import { Collapse, Nav, NavItem, NavbarToggler, Navbar } from 'reactstrap';
+import { Nav, NavItem } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
-import { useState } from 'react';
 
 const NavigationBar = () => {
     
-const [menuOpen, setMenuOpen] = useState(false);
-
   return (
-    <Navbar dark sticky='top' expand='md' className='main-nav'>
-        <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
-            <Collapse isOpen={menuOpen} navbar>
-                <Nav className='pt-3 pb-3' navbar>
-                    <NavItem>
-                        <NavLink className='nav-link' to='/'>
-                            <i className='fa fa-home' /> Home
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className='nav-link' to='/about'>
-                            <i className='fa fa-user' /> About
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className='nav-link' to='/podcast'>
-                            <i className='fa fa-microphone' /> Podcast
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className='nav-link' to='/contact'>
-                            <i className='fa fa-address-card' /> Contact
-                        </NavLink>
-                    </NavItem>
-                </Nav>
-            </Collapse>
-    </Navbar>
+    <Nav className='pt-3 pb-3 justify-content-center' sticky='top' navbar>
+        <NavItem>
+            <NavLink className='nav-link' to='/'>
+                <i className='fa fa-home' /> Home
+            </NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink className='nav-link' to='/about'>
+                <i className='fa fa-user' /> About
+            </NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink className='nav-link' to='/podcast'>
+                <i className='fa fa-microphone' /> Podcast
+            </NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink className='nav-link' to='/contact'>
+                <i className='fa fa-address-card' /> Contact
+            </NavLink>
+        </NavItem>
+    </Nav>
    )
 }
 
