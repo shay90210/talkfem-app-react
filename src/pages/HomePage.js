986918a63f9526c0c7726617b828637f59928be9
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-19 22:12:45 -0500

    Update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,12 @@
 import NavigationBar from '../components/NavigationBar';
 import Header from '../components/Header';
-import SubHeader from '../components/SubHeader';
-import { Container } from 'reactstrap';
 
 const HomePage = () => {
     return (
         <div>
             <NavigationBar />
             <Header/>
-            <Container>
-                <SubHeader />
-            </Container>
-        </div>
+        </div> 
     );
 };
 
