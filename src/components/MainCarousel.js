a894319eb2e600b17518106080bd842fd7a1e098
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-20 13:30:58 -0500

    Carousel

--- src/components/MainCarousel.js
+++ src/components/MainCarousel.js
@@ -5,7 +5,7 @@ import ImageThree from '../img/womantshirt.jpg';
 
 const MainCarousel = () => {
     return (
-        <Carousel>
+        <Carousel fade>
             <Carousel.Item>
                 <img
                 className="d-block w-100"
