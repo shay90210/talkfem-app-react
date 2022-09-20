6ad1097f1ec1083bd1b5eafc9f2ba37d9494c092
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-20 13:05:43 -0500

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,17 @@ import LogoBrand from '../img/new-logo.png';
 const Header = () => {
     return (
         <div className='header'>
-            <img 
-                src={LogoBrand} 
-                alt="podcast logo brand" 
-                width='300px'
-                height='300px'
-                className='mt-5 mb-4 logo-img'
-            />
+            <Header>
+                <img 
+                    src={LogoBrand} 
+                    alt="podcast logo brand" 
+                    width='300px'
+                    height='300px'
+                    className='mt-5 mb-4 logo-img'
+                />
 
-            <h1 className='main-header text-center pb-4'>Talk Your FEMSH!T</h1>
+                <h1 className='main-header text-center pb-4'>Talk Your FEMSH!T</h1>
+            </Header>
         </div>
     )
 }
