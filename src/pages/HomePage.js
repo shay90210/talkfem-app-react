951c7ee940f1725baac2702f83c008861fd2ec19
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-19 22:29:03 -0500

    HomePage and SCSS

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import NavigationBar from '../components/NavigationBar';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const HomePage = () => {
     return (
         <div>
             <NavigationBar />
             <Header/>
+            <Footer />
         </div> 
     );
 };
