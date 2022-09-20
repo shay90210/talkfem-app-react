36c8270f9fcdbd9258393468de08617bb40ebcb2
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-19 21:17:08 -0500

    HomePage and SubHeader

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import './App.css';
 import NavigationBar from './components/NavigationBar';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from '../src/pages/HomePage';
+import AboutPage from '../src/pages/AboutPage';
+import PodcastPage from '../src/pages/PodcastPage';
+import ContactPage from '../src/pages/ContactPage';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
       <Header />
+        <Routes>
+          <Route path='/' element={<HomePage />}/>
+          <Route path='about' element={<AboutPage />}/>
+          <Route path='podcast' element={<PodcastPage />}/>
+          <Route path='contact' element={<ContactPage />}/>
+        </Routes>
       <Footer />
     </div>
   );
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import { Container } from 'reactstrap';
+import SubHeader from '../components/SubHeader';
+
+const HomePage = () => {
+    return (
+        <Container>
+            <SubHeader current='Home' />
+        </Container>
+    );
+};
+
+export default HomePage;
