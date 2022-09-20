653a2cccb77f1504c1114fcdc5294b3bb1a2f770
Shannon Bentley <bentley.shannon@yahoo.com>
2022-09-19 23:15:17 -0500

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
                 alt="podcast logo brand" 
                 width='300px'
                 height='300px'
-                className='mt-5 mb-2 pb-3 logo-img'
+                className='mt-5 mb-4 logo-img'
             />
 
             <h1 className='main-header text-center pb-4'>Talk Your FEMSH!T</h1>
