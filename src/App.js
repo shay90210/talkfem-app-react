afa9ba1e6c3f24f79546b1e1e738a4c2ebea3359
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-01 01:29:33 -0500

    removed about page from app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Background from '../src/img/mainbackground.jpg';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from '../src/pages/HomePage';
-import AboutPage from '../src/pages/AboutPage';
 import PodcastPage from '../src/pages/PodcastPage';
 import ContactPage from '../src/pages/ContactPage';
 import NavigationBar from '../src/components/NavigationBar';
@@ -20,7 +19,6 @@ function App() {
     <NavigationBar />
       <Routes>
         <Route path='/' element={<HomePage />}/>
-        <Route path='/about' element={<AboutPage />}/>
         <Route path='/podcast' element={<PodcastPage />}/>
         <Route path='/contact' element={<ContactPage />}/>
       </Routes>
