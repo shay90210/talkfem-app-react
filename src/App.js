2ea2c6c1b2fcfd8d99e97291a610368cb5432c0a
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-01 01:27:09 -0500

    updated app.js with background image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Background from '../src/img/mainbackground.jpg';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from '../src/pages/HomePage';
 import AboutPage from '../src/pages/AboutPage';
@@ -8,8 +9,15 @@ import NavigationBar from '../src/components/NavigationBar';
 
 function App() {
   return (
-    <div className="App">
-      <NavigationBar />
+    <div 
+      className="App"
+      style={{ 
+        backgroundImage: `url(${Background})`,
+        backgroundSize: 'cover',
+        width: '100%'
+      }}
+    >
+    <NavigationBar />
       <Routes>
         <Route path='/' element={<HomePage />}/>
         <Route path='/about' element={<AboutPage />}/>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,11 @@
 import MainHeader from '../components/MainHeader';
-import MainCarousel from '../components/MainCarousel';
 import Footer from '../components/Footer';
 
 const HomePage = () => {
     return (
         <div>
             <MainHeader/>
-            <MainCarousel />
+
             <Footer />
         </div> 
     );
