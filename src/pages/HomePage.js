c8536e804096f9b691460ce6f1e28ded0f4f9d37
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-24 19:41:27 -0500

    update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,7 +5,7 @@ import { Container, Row, Col, } from 'reactstrap';
 const HomePage = () => {
     return (
         <div className='home-page'>
-            <h1 className='main-header'>TALK YOUR FEMSH!T</h1>
+            <h1 className='main-header pt-4'>TALK YOUR FEMSH!T</h1>
             <h2 className='second-header'>/tôk/ /yôr,yo͝or/ /fem/ /SHit/</h2>
             <h3 className='third-header'>a safe space for women to talk shit.</h3>
             <Container>
