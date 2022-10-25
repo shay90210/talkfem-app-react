73c9cf31bffd84abd502edc7eccbffffddf66067
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-24 20:10:34 -0500

    update form

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -16,7 +16,8 @@ const ContactPage = () => {
             <div 
                 style={{ 
                     backgroundColor: 'white',
-                    marginTop: '70px'
+                    marginTop: '70px',
+                    marginBottom: '70px'
                 }}>
                 <p
                     style={{
@@ -25,17 +26,73 @@ const ContactPage = () => {
                         paddingTop: '30px'
                     }}
                 >Contact us for specific topics, advice, or if you would like to join our show!</p>
-                <Form>
+                <Form
+                    style={{
+                        paddingBottom: '30px',
+                    }}
+                >
                     <FormGroup row>
-                        <Label for='firstName' sm={2}>
+                        <Label for='firstName'>
                             First Name 
                         </Label>
-                        <Col sm={10}>
+                        <Col sm={8}>
                             <Input 
                                 id='firstName'
                                 name='name'
                                 placeholder='First Name'
                                 type='text'
+                            />                    
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label for='lastName'>
+                            Last Name 
+                        </Label>
+                        <Col sm={8}>
+                            <Input 
+                                id='lastName'
+                                name='name'
+                                placeholder='Last Name'
+                                type='text'
+                            />                    
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label for='emailAddress'>
+                            Email
+                        </Label>
+                        <Col sm={8}>
+                            <Input 
+                                id='emailAddress'
+                                name='email'
+                                placeholder='Email'
+                                type='email'
+                            />                    
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label for='phoneNum'>
+                            Phone Number
+                        </Label>
+                        <Col sm={8}>
+                            <Input 
+                                id='phoneNum'
+                                name='phoneNum'
+                                placeholder='Phone Number'
+                                type='phoneNum'
+                            />                    
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label for='messageArea'>
+                            Message
+                        </Label>
+                        <Col sm={8}>
+                            <Input 
+                                id='messageText'
+                                name='text'
+                                placeholder='Leave a message here'
+                                type='textarea'
                             />                    
                         </Col>
                     </FormGroup>
