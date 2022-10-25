b9109dcd4825ee623defbf5f5c1d34cce7adc3b0
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-24 19:35:21 -0500

    update

--- src/pages/EpisodesPage.js
+++ src/pages/EpisodesPage.js
@@ -1,8 +1,12 @@
 
 const EpisodesPage = () => {
-    <div>
-        <h1>Talke your sdfsaf</h1>
-    </div>
+    return (
+        <div className='episodes'>
+            <h1 className='main-header'>sex·u·al·i·ty</h1>
+            <h2 className='second-header'>/ˌsekSHo͞oalədē/</h2>
+            <h3 className='third-header'>noun: capacity for sexual feelings.</h3>
+        </div>
+    )
 }
 
 export default EpisodesPage;
--- src/pages/ListenLivePage.js
+++ src/pages/ListenLivePage.js
@@ -2,8 +2,10 @@
 
 const ListenLivePage = () => {
     return (
-        <div>
+        <div className='listen-live'>
             <h1 className='main-header'>fem·i·nism</h1>
+            <h2 className='second-header'>/ˈfeməˌnizəm/</h2>
+            <h3 className='third-header'>noun: the advocacy of women's rights on the basis of the equality of the sexes.</h3>
         </div>
     )
 }
