da8b6dc7d05e06b230bf4da1f309ee7647454c51
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-25 17:43:50 -0500

    set up outline for audio player

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,10 +1,27 @@
+import { Button, Input } from "reactstrap";
+
 const AudioPlayer = () => {
 
     return (
         <div>
-            <audio>
+            <audio src='/'preload='metadata'></audio>
 
-            </audio>
+            <Button>back 30</Button>
+            <Button>play / pause</Button>
+            <Button>forward 30</Button>
+
+            {/* current time */}
+            <div>0:00</div>
+
+            {/* progress bar */}
+            <div>
+                <Input 
+                    type='range'
+                />
+            </div>
+
+            {/* duration */}
+            <div>2:49</div>
         </div>
     )
 }
