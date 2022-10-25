74b75a7a7331d7bc1db499a78bce0a851c834290
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-24 19:47:08 -0500

    update titles styling

---
+++ src/pages/ContactPage.js
@@ -0,0 +1,11 @@
+const ContactPage = () => {
+    return (
+        <div className='contact'>
+            <h1 className='main-header'>wom·an·ism</h1>
+            <h2 className='second-header'>/ˈwo͝oməˌnizəm/</h2>
+            <h3 className='third-header'>a form of feminism that emphasizes women's natural contribution to society.</h3>
+        </div>
+    )
+}
+
+export default ContactPage; 
