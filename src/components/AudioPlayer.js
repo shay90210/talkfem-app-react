86f5246b404ae883a74f5e98ddb4cfd292c1281f
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-25 17:59:15 -0500

    added fa icons and completed use state funtion

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,14 +1,31 @@
 import { Button, Input } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { 
+    faArrowAltCircleLeft,
+    faArrowAltCircleRight,
+    faPlayCircle,
+    faPauseCircle
+} from "@fortawesome/free-solid-svg-icons";
 
 const AudioPlayer = () => {
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const togglePlayPause = () => {
+        setIsPlaying(!isPlaying);
+    }
 
     return (
-        <div>
+        <div cLassName='audio-player'>
             <audio src='/'preload='metadata'></audio>
 
-            <Button>back 30</Button>
-            <Button>play / pause</Button>
-            <Button>forward 30</Button>
+            <Button><FontAwesomeIcon icon={faArrowAltCircleLeft} /> 30</Button>
+
+            <Button onClick={togglePlayPause}>
+                { isPlaying ? <FontAwesomeIcon icon={faPauseCircle} /> : <FontAwesomeIcon icon={faPlayCircle} /> }
+            </Button>
+
+            <Button>30 <FontAwesomeIcon icon={faArrowAltCircleRight} /></Button>
 
             {/* current time */}
             <div>0:00</div>
