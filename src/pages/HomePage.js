171fc989616515bf4d445a0ee79aab76df0ea6ac
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-25 20:40:55 -0500

    update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,8 +3,6 @@ import BrandLogo from '../img/logo.png';
 import { 
     Container, 
     Row, 
-    Col, 
-    Card 
 } from 'reactstrap';
 
 const HomePage = () => {
@@ -12,25 +10,16 @@ const HomePage = () => {
         <div className='home-page'>
             <h1 className='main-header pt-4'>TALK YOUR FEMSH!T</h1>
             <h2 className='second-header'>/tôk/ /yôr,yo͝or/ /fem/ /SHit/</h2>
-            <h3 className='third-header'>a safe space for women to talk shit.</h3>
-            <Container>
-                <Row>
-                    <Col>
+            <h3 className='third-header'>a safe space where we uplift women and talk shit.</h3>
+                <Container>
+                    <Row>
                         <img 
                             src={BrandLogo}
                             className='brand-logo'
                             alt='the podcast logo'
                         />
-                    </Col>
-                    <Col>
-                        <div>
-                            <Card>
-                                Add Something Here
-                            </Card>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
+                    </Row>
+                </Container>
         </div>
     );
 };
