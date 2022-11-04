076839ad8a32bcdbbb9d6d830930dde90e96d30f
Shannon Bentley <bentley.shannon@yahoo.com>
2022-11-03 20:11:45 -0500

    updated styles and added a button to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,12 +3,16 @@ import BrandLogo from '../img/logo.png';
 import { 
     Container, 
     Row, 
-    Col
+    Col,
+    Button
 } from 'reactstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeadphones } from '@fortawesome/free-solid-svg-icons';
 
 const HomePage = () => {
     return (
-        <div className='home-page'>
+        <div className='home-page pb-5'>
             <h1 className='main-header pt-4'>TALK YOUR FEMSH!T</h1>
             <h2 className='second-header'>/tôk/ /yôr,yo͝or/ /fem/ /SHit/</h2>
             <h3 className='third-header'>a safe space where we uplift women and talk shit.</h3>
@@ -20,6 +24,13 @@ const HomePage = () => {
                                 className='brand-logo'
                                 alt='the podcast logo'
                             />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Button href='/live' className='live-button'>
+                                Listen Now <FontAwesomeIcon icon={faHeadphones} />
+                            </Button>
                         </Col>
                     </Row>
                 </Container>
